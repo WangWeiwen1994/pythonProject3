f3b537f46ca256c6d714475fb1aa1eb924e2457f
WangWeiwen1994 <285675625@qq.com>
2023-01-20 13:23:29 +0800

    #AUTO_ACCOUNT-2 #main.py注释优化和本地调用文件新建#https://euzo20220823230918828.pingcode.com/pjm/items/63ca23e3dd44b33fde6bd935? #AUTO_ACCOUNT-2 main.py注释优化和本地调用文件新建#

--- main.py
+++ main.py
@@ -31,13 +31,16 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--Environment", help="choose the Environment", required=True)
     # -t 选择要计算的记账日期
     parser.add_argument("-t", "--Time", help="choose the Time", required=True)
-    # -u 选择要计算的记账日期
+    # -u 选择要计算的用户
     parser.add_argument("-u", "--User", help="choose the Time", required=True)
-    # -at 选择要计算的记账日期
+    # -at 选择要计算的记账模式
     parser.add_argument("-at", "--aapz_number_type", help="choose the type 'default' or 'bank_in_day'", required=True, default='default')
     args = vars(parser.parse_args())
-    # 读取的配置文件为'config.ini'
+
+    # 向data类中传入除analyzer外的全部配置参数
     data = Data(filename=args['Environment'], date=args['Time'], user=args['User'], aapz_number_type=args['aapz_number_type'] )
+
+    # 逐个执行传入的analyer分析类
     for analyer_execution in Analyzers:
         if analyer_execution.__name__ == args['Analyzer']:
             analyer_execution(data).analyzer()
